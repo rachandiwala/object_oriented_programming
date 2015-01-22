9ead555c6775ce24edfd251baae90a73baa8a6a3
Raza Chandiwala <rachandiwala@gmail.com>
2015-01-22 17:08:11 -0500

    OOP: Exesrcise 1: Class Time. Practice on OOP class objects, inheritance and variables

---
+++ people.rb
@@ -0,0 +1,51 @@
+class Person 
+
+attr_accessor :name
+	
+	def initialize(name)
+		@name= name
+	end
+
+	def greeting
+		puts "Hi, my name is #{name}"
+    end
+end
+
+
+class Student < Person
+	
+	def learn
+		puts "I get it!"
+	end
+
+end
+
+
+class Instructor < Person 
+	def teach
+		puts "Everything in Ruby is an Object"
+	end
+end
+
+
+# i is for instructors
+
+i=Instructor.new("")
+i.greeting
+i.teach
+
+# s is for Students
+
+s = Student.new("Cristina")
+s.greeting
+s.learn
+
+
+s.teach
+=begin
+	
+The above line does not work because I am in Students 
+class and I am calling something from the Instructors class. 
+They have no inheritance set up between them. 
+
+=end
